3e806c0aec242aa122d2e06946d83a0498c95da6
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-09-19 01:04:30 -0400

    Initial Commit

---
+++ Hiking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by Jimmy Ghelani on 2019-09-19.
+//  Copyright © 2019 JGDesign Solutions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
